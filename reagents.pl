2f88b9647fad1574cf10dc843dc05fb029dc08ae
Spencer Rathbun <srathbun@riverainc.com>
2012-11-27 11:50:41 -0500

    added find and rename regex, it uses one e so that it does not eval twice

--- reagents.pl
+++ reagents.pl
@@ -4,3 +4,5 @@
 # /ee means eval replacement
 # note the curly brackets around the var name, since it is not interpolated by the regex match
 perl -e '$_="/home/USER/PATH/SERVER/__Tue Nov 27 01:38:51 EST 2012__logs.txt"; s/(.*)(__)(.*)(__)(.*)/$var=parsedate($3); $_="$1__${var}__$5"; print "__$5\n"/ee;'
+
+find /home/username/uploads/ -type f -name '*__*__*' -exec rename 's/(.*)(__)(.*)(__)(.*)/use Time::ParseDate; my($var)=parsedate($3); $_="$1__${var}__$5";/e' "{}" \;
