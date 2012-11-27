2ab5cd1da22c133ce036291abe84a09b48c34fb6
Spencer Rathbun <srathbun@riverainc.com>
2012-11-27 10:25:59 -0500

    Added regex for eval during replacement

---
+++ reagents.pl
@@ -0,0 +1,6 @@
+
+
+# grab date in filename, then convert to epoch time
+# /ee means eval replacement
+# note the curly brackets around the var name, since it is not interpolated by the regex match
+perl -e '$_="/home/USER/PATH/SERVER/__Tue Nov 27 01:38:51 EST 2012__logs.txt"; s/(.*)(__)(.*)(__)(.*)/$var=parsedate($3); $_="$1__${var}__$5"; print "__$5\n"/ee;'
